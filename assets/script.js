f49e3cd37a2de209a2e89384363503788d49bd36
Van Gogh <139753682+CircuitSorcerer@users.noreply.github.com>
2023-09-15 00:58:08 +1000

    More tweaks to game functions, not quite working

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,9 @@ function timer() {
             timerEl.textContent = secondsLeft + " secs";
         } else {
             clearInterval();
+            mainEl.style.display = "none";
+        document.getElementById("container").appendChild(messageEl)
+        messageEl.textContent = "YOU LOSE!"
         }
         
 
@@ -25,18 +28,34 @@ timer()
 // Game Functions
 
 const questions = [
-    q1 = {"question":"Which of the keyword is used to define the variable in the javascript?", 1:"var",2:"let",3:"Both A & B", 4:"None of the above", correct: 3},
-    q2 = {},
-    q3 = {},
-    q4 = {}
+    q1 = {"question":"Which of the keyword is used to define the variable in the javascript?", 1:"var",2:"let",3:"Both A & B", 4:"None of the above", "correct": 3},
+    q2 = {"question":"How do we put Javascript inside HTML?", 1:"<js>",2:"<javascript>",3:"<scripting>", 4:"<script>", "correct": 4},
+    q3 = {"question":"The 'let' and 'var' are known as:", 1:"Prototypes",2:"Declaration Statements",3:"Data Types", 4:"Keywords", "correct": 2},
+    q4 = {"question":"Which one is not a comparison operator?", 1:"=",2:"<",3:">", 4:"!=", "correct": 1}
 ]
 
 var gameEl = document.createElement("div");
-var obj = 0
+var questionEl = document.createElement("h3");
+var messageEl = document.createElement("h2")
+var wrapperEl = document.getElementById("container")
+var points = 0;
+var obj = 0;
+
+
+function questionSelector() {
+    if (obj < questions.length) {
+        obj += 1;
+    } else {
+        mainEl.style.display = "none";
+        wrapperEl.appendChild(messageEl)
+        messageEl.textContent = "YOU WIN!"
+    }
+    gameDisplay();
+}
 
 function gameDisplay() {
     
-    var questionEl = document.createElement("h3");
+    
     
     questionEl.textContent = questions[obj].question;
     mainEl.appendChild(gameEl);
@@ -56,18 +75,39 @@ gameEl.addEventListener("click", function(event) {
     event.preventDefault();
     event.stopPropagation();
 
-    var answer = Object.keys(questions[obj]).find(key => questions[obj][key] === event.currentTarget.textContent);
-    console.log(event.currentTarget.textContent)
-    console.log(answer)
-    let correctAns = questions[obj][correct]
-    if (answer !== correctAns) {
+    var answer = Object.keys(questions[obj]).find(key => questions[obj][key] === event.target.textContent);
+    console.log(questions[obj]["correct"]);
+    console.log(answer);
+    let correctAns = questions[obj]["correct"];
+    if (answer != correctAns) {
         console.log("incorrect");
+        secondsLeft -= 10;
+        questionEl.remove();
+        //buttonEl.remove();
+        if (obj == questions.length-1) {
+            mainEl.style.display = "none";
+            wrapperEl.appendChild(messageEl)
+            messageEl.textContent = "YOU LOSE!"
+        } else {
+            questionSelector();
+        }
+        
     } else {
         console.log("correct");
+        questionEl.remove();
+        //buttonEl.remove();
+        questionSelector();
+        points += 5;
+        if (obj == questions.length-1) {
+            mainEl.style.display = "none";
+            wrapperEl.appendChild(messageEl)
+            messageEl.textContent = "YOU WIN!"
+        } else {
+            questionSelector();
+        }
     }
 
 })
-
 
 
 gameDisplay();
